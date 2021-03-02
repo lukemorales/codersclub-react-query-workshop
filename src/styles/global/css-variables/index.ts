8aba11ca52a0278ba9ce594712719cefd27b4847
Luke Morales <lukemorales@live.com>
2021-03-02 00:18:57 -0300

    chore: improve colors for dark mode

--- src/styles/global/css-variables/index.ts
+++ src/styles/global/css-variables/index.ts
@@ -26,7 +26,7 @@ export const THEME_CSS_VARIABLES = css`
     --color-background: #151619;
 
     --color-accent-primary: #e71d60;
-    --color-accent-primary-soft: #fde8ef;
+    --color-accent-primary-soft: #472a33;
 
     --color-accent-secondary: #334168;
 
@@ -36,7 +36,7 @@ export const THEME_CSS_VARIABLES = css`
     --color-text-highlight: #ffffff;
 
     --color-stars-fill: #fa7c06;
-    --color-stars-empty: #dae5ff;
+    --color-stars-empty: #6e727a;
 
     --color-border: #2c2e33;
     --color-search-bar: #f1f7ff;
